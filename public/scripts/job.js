f6925392ae4a57add18be72d315e18a75d1cc7b0
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-04-17 09:59:05 -0300

    feat: calculate remaining hours, minutes

---
+++ public/scripts/job.js
@@ -0,0 +1,11 @@
+const nameInput = document.getElementById("name");
+const submitBtn = document.getElementById("submit-btn");
+const dailyHoursInput = document.getElementById("daily-hours");
+const totalHoursInput = document.getElementById("total-hours");
+
+// submitBtn.addEventListener("click", el => {
+//   // el.preventDefault();
+//   if (dailyHoursInput.value > totalHoursInput.value) {
+//     alert("Horas diárias não pode ser maior que total de horas!");
+//   }
+// });
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,24 @@ const routes = express.Router();
 
 const views = `${__dirname}/views/`;
 
-const profile = {
-  name: "Luis",
-  avatar: "https://github.com/capelaum.png",
-  "monthly-budget": 1200,
-  "days-per-week": 5,
-  "hours-per-day": 6,
-  "vacation-per-year": 2,
-  "value-hour": 75,
+const Profile = {
+  data: {
+    name: "Luis",
+    avatar: "https://github.com/capelaum.png",
+    "monthly-budget": 1200,
+    "days-per-week": 5,
+    "hours-per-day": 6,
+    "vacation-per-year": 2,
+    "value-hour": 75,
+  },
+
+  controllers: {
+    index(req, res) {
+      return res.render(`${views}profile`, { profile: Profile.data });
+    },
+
+    update(req, res) {},
+  },
 };
 
 const Job = {
@@ -19,7 +29,7 @@ const Job = {
       id: 1,
       name: "Pizzaria Guloso",
       "daily-hours": 2,
-      "total-hours": 1,
+      "total-hours": 2,
       created_at: Date.now(),
     },
     {
@@ -29,81 +39,141 @@ const Job = {
       "total-hours": 40,
       created_at: Date.now(),
     },
+    {
+      id: 2,
+      name: "Away Project",
+      "daily-hours": 5,
+      "total-hours": 2,
+      created_at: Date.now(),
+    },
   ],
-  
-  controllers: {
 
+  controllers: {
     index(req, res) {
       const updatedJobs = Job.data.map(job => {
-        const remainingDays = Job.services.calculateRemainingDays(job);
-        const status = remainingDays <= 0 ? "done" : "progress";
-    
+        const remainingTime = Job.services.calculateRemainingTime(job);
+        let deadline, status;
+
+        if(remainingTime.hoursLeft && remainingTime.minutesLeft) {
+          deadline = `${remainingTime.hoursLeft}h ${remainingTime.minutesLeft}m`
+        } else {
+          deadline = `${remainingTime.daysLeft}d`
+        }
+
+        if(remainingTime.minutesLeft) {
+          status = remainingTime.minutesLeft <= 0 ? "done" : "progress";
+        } else {
+          status = "progress";
+        }
+
         return {
           ...job,
-          remainingDays,
+          deadline,
           status,
-          budget: profile["value-hour"] * job["total-hours"],
+          budget: Profile.data["value-hour"] * job["total-hours"],
         };
       });
-    
+
       return res.render(`${views}index`, { jobs: updatedJobs });
     },
 
     create(req, res) {
-      return res.render(`${views}job`)
+      return res.render(`${views}job`);
     },
 
     save(req, res) {
       // req.body: { name: 'Luis', 'daily-hours': '3.3', 'total-hours': '2' }
+      const isValidJob = Job.controllers.validateJob(req.body);
+      
+      if(!isValidJob) {
+        const errorMsg = "Job Inválido! Estimativa não pode ser menor que horas diárias para o Job";
+        return res.redirect(`/?${errorMsg}`)
+      }
+      
+      if(isValidJob) {
+        // const lastId = Job.data[Job.data.length - 1]?.id || 1;
+        const lastId = Job.data.length + 1;
 
-      // const lastId = Job.data[Job.data.length - 1]?.id || 1;
-      const lastId = Job.data.length + 1;
-
-      Job.data.push({
-        id: lastId,
-        name: req.body.name,
-        "daily-hours": req.body["daily-hours"],
-        "total-hours": req.body["total-hours"],
-        created_at: Date.now(),
-      });
+        Job.data.push({
+          id: lastId,
+          name: req.body.name,
+          "daily-hours": req.body["daily-hours"],
+          "total-hours": req.body["total-hours"],
+          created_at: Date.now(),
+        });
+      }
 
       return res.redirect("/");
-    }
-  },
-  
-  services: {
-    calculateRemainingDays(job) {
-      // poe na forma de Data: Sat Apr 17 horario..
-      const createdDate = new Date(job.created_at);
-      const remainingDays = Math.ceil(job["total-hours"] / job["daily-hours"]);
-    
-      // soma dia da data de criação mais dias necessarios para entrega: 27
-      const dueDay = createdDate.getDate() + Number(remainingDays);
-    
-      // poe data de entrega em milisegundos
-      const dueDateMs = createdDate.setDate(dueDay);
-    
-      // subtrai data de entrega com data atual em milisegundos
-      const timeDiffMs = dueDateMs - Date.now();
-    
-      // dia em milisegundos
-      const dayMs = 1000 * 60 * 60 * 24;
-    
-      // transforma para dias restantes
-      const dayDiff = Math.floor(timeDiffMs / dayMs);
-    
-      return dayDiff;
+    },
+
+    validateJob(job) {
+
+      if (job["daily-hours"] > job["total-hours"]) {
+        console.log(job["daily-hours"] , job["total-hours"]);
+        return false;
+      }
+      return true;
     },
   },
-}
+
+  services: {
+    calculateRemainingTime(job) {
+      // poe na forma de Data: Sat Apr 17 horario..
+      const createdDate = new Date(job.created_at);
+
+      const remainingDays = Math.floor(job["total-hours"] / job["daily-hours"]);
+      console.log("🚀 ~ remainingDays: ", remainingDays);
+
+      // soma dia da data de criação mais dias necessarios para entrega
+      const dueDay = createdDate.getDate() + Number(remainingDays);
+
+      // poe data de entrega em milisegundos
+      const dueDateMs = createdDate.setDate(dueDay);
+
+      // subtrai data de entrega com data atual em milisegundos
+      const timeDiffMs = dueDateMs - Date.now();
+      
+      const dayMs = 1000 * 60 * 60 * 24;
+      const hourMs = 1000 * 60 * 60;
+      const minuteMs = 1000 * 60;
+      
+      // transforma para dias restantes
+      const daysLeft = Math.floor(timeDiffMs / dayMs);
+      console.log("🚀 ~ daysLeft", daysLeft);
+
+      if(daysLeft < 1) {
+        const dueTime = job.created_at + (job["total-hours"] * hourMs);
+        const timeLeft = dueTime - Date.now();
+        console.log("🚀 ~ timeLeft", timeLeft)
+        
+        const hoursLeft = Math.floor((timeLeft / hourMs) % 24);
+        console.log("🚀 ~ hoursLeft", hoursLeft);
+        
+        const minutesLeft = Math.floor((timeLeft / minuteMs) % 60);
+        console.log("🚀 ~ minutesLeft", minutesLeft);
+
+        const dayDiff = 0;
+        return {
+          dayDiff,
+          hoursLeft,
+          minutesLeft
+        };
+      }
+      
+      console.log("-----------------");
+      return {
+        daysLeft,
+      };
+      
+    },
+  },
+};
 
 routes.get("/", Job.controllers.index);
 routes.get("/job", Job.controllers.create);
 routes.post("/job", Job.controllers.save);
 
 routes.get("/job/edit", (req, res) => res.render(`${views}job-edit`));
-routes.get("/profile", (req, res) =>
-  res.render(`${views}profile`, { profile })
-);
+routes.get("/profile", Profile.controllers.index);
 
 module.exports = routes;
