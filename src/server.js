15b1cb2987285986d56b274336c0777795434678
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-04-17 00:44:41 -0300

    set ejs template engine

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 
-server.use(express.static("public"));
+server.set('view engine', 'ejs');
 
+server.use(express.static("public"));
 server.use(routes);
+
 server.listen(3000, () => console.log("rodando"));
