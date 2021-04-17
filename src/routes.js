3a55cdd3ed5c7d05cbce3f274e86337e5361d437
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-04-17 01:11:43 -0300

    set profile object

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,20 @@ const routes = express.Router();
 
 const views = `${__dirname}/views/`;
 
+const profile = {
+  name: "Luis",
+  avatar: "https://avatars.githubusercontent.com/u/37607384?v=4",
+  "monthly-budget": 1200,
+  "days-per-week": 5,
+  "hours-per-day": 6,
+  "vacation-per-year": 2
+}
+
 routes.get("/", (req, res) => res.render(`${views}index`));
 routes.get("/job", (req, res) => res.render(`${views}job`));
 routes.get("/job/edit", (req, res) => res.render(`${views}job-edit`));
-routes.get("/profile", (req, res) => res.render(`${views}profile`));
+routes.get("/profile", (req, res) => res.render(`${views}profile`, { profile }));
+
+
 
 module.exports = routes;
