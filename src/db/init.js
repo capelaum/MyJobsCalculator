b3a1c70c6c9cca475018c637a1b18ba5254feca4
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-04-18 22:30:38 -0300

    config database

--- src/db/init.js
+++ src/db/init.js
@@ -1,7 +1,76 @@
-const Database = require('config');
+const Database = require("./config");
 
-Database();
+const initDb = {
+  async init() {
 
-Database.exec();
+    const db = await Database();
 
-Database.close();
+    await db.exec(`CREATE TABLE IF NOT EXISTS profile (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        avatar TEXT,
+        monthly_budget INT,
+        days_per_week INT,
+        hours_per_day INT,
+        vacation_per_year INT,
+        value_hour INT
+      );`
+    );
+
+    await db.exec(`CREATE TABLE IF NOT EXISTS jobs(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        daily_hours INT,
+        total_hours INT,
+        created_at DATETIME
+      );`
+    );
+
+    await db.run(`INSERT INTO profile (
+        name, 
+        avatar, 
+        monthly_budget, 
+        days_per_week, 
+        hours_per_day, 
+        vacation_per_year
+      ) VALUES (
+        "Luis",
+        "https://github.com/capelaum.png",
+        1200,
+        5,
+        6,
+        4
+      );`
+    );
+
+    await db.run(`INSERT INTO jobs (
+      name, 
+      daily_hours, 
+      total_hours, 
+      created_at
+      ) VALUES (
+        "Artur Orgânicos",
+        2,
+        50,
+        1618780267510
+      );`
+    );
+
+    await db.run(`INSERT INTO jobs (
+      name, 
+      daily_hours, 
+      total_hours, 
+      created_at
+      ) VALUES (
+        "Carol Website",
+        2,
+        30,
+        1618780267510
+      );`
+    );
+
+    await db.close();
+  },
+}
+
+initDb.init();
