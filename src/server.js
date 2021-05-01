54079d40d042e293caa914b99dae51de2c8779c7
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-05-01 13:49:44 -0300

    config deploy server

--- src/server.js
+++ src/server.js
@@ -10,4 +10,6 @@ server.use(express.static("public"));
 server.use(express.urlencoded({ extended: true })); // usar o req.body
 server.use(routes);
 
-server.listen(3000, () => console.log("rodando"));
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log("rodando"));
