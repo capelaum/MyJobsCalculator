75417d283de1b9c58644c579f0ee53e88519fafe
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-05-01 14:15:38 -0300

    move public folder

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ server.set('views', path.join(__dirname, 'views'));
 // var publicDir = path.join(__dirname, '../public');
 // server.use(express.static(publicDir));
 
-server.use(express.static("../public"));
+server.use(express.static("./public"));
 server.use(express.urlencoded({ extended: true })); // usar o req.body
 server.use(routes);
 
